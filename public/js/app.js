ee9815299b5f1b343183d35500abd7ce2c08dbef
caitypizza <caitykondo@gmail.com>
2016-12-17 19:32:05 -1000

    created h2 titles for each article

--- public/js/app.js
+++ public/js/app.js
@@ -8,13 +8,23 @@ function reqListener() {
   console.log(article.length);
 
   for (var i = article.length - 1; i >= 0; i--) {
+    // New Container
     let articleContainer = document.createElement('div');
     articleContainer.className ='articleContainer';
     body.appendChild(articleContainer);
+
+    // Image
     let articleImageURL = article[i].data.preview.images[0].source.url;
     let articleImage = document.createElement('img');
     articleContainer.appendChild(articleImage);
     articleImage.src = articleImageURL;
+
+    // Headline
+    let articleTitle = document.createElement('h2')
+    articleTitle.innerHTML = article[i].data.title;
+    articleContainer.appendChild(articleTitle);
+    // By, Posted, Views
+    // Description
   }
 }
 let oReq = new XMLHttpRequest();
