20df61e41651a93c31e4128bb07797c51528de65
caitypizza <caitykondo@gmail.com>
2016-12-17 19:49:22 -1000

    added date and author to each article

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ function reqListener() {
     articleContainer.className ='articleContainer';
     body.appendChild(articleContainer);
 
-    // Image
+    // Image >> PUT A FALLBACK DEFAULT IMAGE LATER
     let articleImageURL = article[i].data.preview.images[0].source.url;
     let articleImage = document.createElement('img');
     articleContainer.appendChild(articleImage);
@@ -23,7 +23,26 @@ function reqListener() {
     let articleTitle = document.createElement('h2')
     articleTitle.innerHTML = article[i].data.title;
     articleContainer.appendChild(articleTitle);
-    // By, Posted, Views
+
+    // Author
+    let articleAuthor = document.createElement('span');
+    articleAuthor.className = 'articleAuthor';
+    articleAuthor.innerHTML = article[i].data.author;
+    articleContainer.appendChild(articleAuthor);
+
+    // Date
+    let articleCreated = document.createElement('span');
+    articleCreated.className = 'articleCreated';
+    articleCreated.innerHTML = article[i].data.created;
+    let date = article[i].data.created;
+    articleContainer.appendChild(articleCreated);
+
+    // Comments
+    let articleComments = document.createElement('span');
+    articleComments.className = 'articleComments';
+    articleComments.innerHTML = article[i].data.num_comments;
+    articleContainer.appendChild(articleComments);
+
     // Description
   }
 }
